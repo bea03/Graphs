ffe358b3918621f3485d618dca7f652891a6e76e
Nicole Beamer <tulip@Nicoles-iMac.hsd1.ut.comcast.net>
2020-07-16 09:51:07 -0600

    stuff

---
+++ island.py
@@ -0,0 +1,22 @@
+'''
+And also, try solving the islands matrix problem, using graphs!
+Islands Matrix Problem
+
+Write a function that takes a 2D binary array and returns the number of 1 islands. 
+An island consists of 1s that are connected to the north, south, east or west. For example:
+
+islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+island_counter(islands) # returns 4
+what are nodes: 1
+edges: connected n/s/e/w
+
+an island = 1s cluster connected by nsew (connected components)
+
+build graph, def get neighbors()
+
+
+'''
